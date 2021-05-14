7f758139aa900ebe5b2c3a627ef1abb90d8cf509
Crowdbotics <team@crowdbotics.com>
2021-05-14 12:07:19 +0000

    Update components

---
+++ screens/Android-1(38:3)0231649/index.js
@@ -0,0 +1,93 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View style={styles.View_38_4} />
+      <View style={styles.View_38_6} />
+      <View style={styles.View_38_7} />
+      <View style={styles.View_2} />
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  ScrollView_1: {},
+  View_38_4: {
+    width: wp("100%"),
+    minWidth: wp("100%"),
+    height: hp("7.5%"),
+    minHeight: hp("7.5%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("1.09375%"),
+    backgroundColor: "rgba(77, 112, 237, 1)"
+  },
+  View_38_6: {
+    width: wp("77.5%"),
+    minWidth: wp("77.5%"),
+    height: hp("5.625%"),
+    minHeight: hp("5.625%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("19.72222222222222%"),
+    top: hp("2.03125%"),
+    backgroundColor: "rgba(255, 255, 255, 1)",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10
+  },
+  View_38_7: {
+    width: wp("17.77777777777778%"),
+    minWidth: wp("17.77777777777778%"),
+    height: hp("5.625%"),
+    minHeight: hp("5.625%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("2.03125%"),
+    backgroundColor: "rgba(255, 255, 255, 1)",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10
+  },
+  View_2: { height: 640 }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
